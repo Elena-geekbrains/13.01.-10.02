7f687f0bf65667fdea40ab914da859c29e83f69f
Elena <0078122446925@mail.ru>
2023-01-15 14:15:11 +0300

    Added Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,38 +1,22 @@
-﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-// 3 -> Среда
-// 5 -> Пятница
+﻿// Задача 2
+// Напишите программу, которая:
+// 1. На вход принимает два числа.
+// 2. Выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите число: ");
-string day = Console.ReadLine();
-if (day == "1")
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
 {
-    Console.WriteLine("Понедельник");
-}
-else if (day == "2")
-{
-    Console.WriteLine("Вторник");
-}
-else if (day == "3")
-{
-    Console.WriteLine("Среда");
-}
-else if (day == "4")
-{
-    Console.WriteLine("Четверг");
-}
-else if (day == "5")
-{
-    Console.WriteLine("Пятница");
-}
-else if (day == "6")
-{
-    Console.WriteLine("Суббота");
-}
-else if (day == "7")
-{
-    Console.WriteLine("Воскресенье");
+    Console.WriteLine($"Первое число ({number1}) больше второго числа ({number2}), а второе число ({number2}) в саою очередь меньше первого числа ({number1}).");
 }
 else 
 {
-    Console.WriteLine("Ошибка ввода");
+    Console.WriteLine($"Первое число ({number1}) меньше второго числа ({number2}), а второе число ({number2}) в саою очередь больше первого числа ({number1}).");
 }
