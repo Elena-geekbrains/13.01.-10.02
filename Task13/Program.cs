b44ca48885dc53f920fa1586bb1cd6c9e19e5804
Elena <0078122446925@mail.ru>
2023-01-20 07:58:32 +0300

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 13
+Напишите программу, которая:
+1. выводит третью цифру заданного числа
+2. или сообщает, что третьей цифры нет. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int ThirtDigit(int num)
+{
+    return num = num / 100 % 10;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number < 100) Console.WriteLine($"В введенном числе нет третьей цифры!");
+else if (number < 1000) number = number % 10;
+else number = ThirtDigit(number);
+Console.WriteLine($"Третья цифра введенного числа => {number}");
+
+/* Решение с использованием дополнительной переменной result:
+
+int ThirtDigit(int num)
+{
+    return num = num / 100 % 10;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number < 100) Console.WriteLine($"В введенном числе нет третьей цифры!");
+else if (number < 1000)
+{
+    int result = number % 10;
+    Console.WriteLine($"Третья цифра введенного числа => {result}");
+}
+
+else
+{
+    int result = ThirtDigit(number);
+    Console.WriteLine($"Третья цифра введенного числа => {result}");
+}
+*/
